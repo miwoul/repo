8cbec5acd1963f6d0a30fbb5f5100e4d10148a70
Miwoul <pjh200305@gmail.com>
2024-09-04 23:04:29 +0900

    Feat: "mapper CRUD"

--- src/main/java/com/work/todolist/todolist/domain/Todo.java
+++ src/main/java/com/work/todolist/todolist/domain/Todo.java
@@ -3,7 +3,14 @@ package com.work.todolist.todolist.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class Todo {
+    private Long id;
+    private String dateTime;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/work/todolist/todolist/mapper/TodoMapper.java
+++ src/main/java/com/work/todolist/todolist/mapper/TodoMapper.java
@@ -1,7 +1,21 @@
 package com.work.todolist.todolist.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.work.todolist.todolist.domain.Todo;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
-public class TodoMapper {
+public interface TodoMapper {
+    @Select("SELECT * FROM todo")
+    List<Todo> allTodo();
+    @Select("SELECT * FROM todo WHERE date_time=#{dateTime}")
+    List<Todo> todoList(String dateTime);
+    @Insert("INSERT INTO todolist (date_time, title, content, created_at) values (#{dateTime}, #{title}, #{content}, NOW())")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void addTodo(Todo todo);
+    @Update("UPDATE todo SET date_time=#{dateTime}, title=#{title}, content=#{content} WHERE id=#{id}")
+    void updateTodo(Todo todo);
+    @Delete("DELETE FROM todo WHERE id=#{id}")
+    void deleteTodo(Long id);
 }
