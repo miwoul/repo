45728834c85dd29842fa13ed5a50be115d5560c4
miwoul <pjh200305@gmail.com>
2024-09-05 11:50:18 +0900

    Feat: "기본적인 CRUD 완성"

--- src/main/java/com/work/todolist/todolist/mapper/TodoMapper.java
+++ src/main/java/com/work/todolist/todolist/mapper/TodoMapper.java
@@ -11,7 +11,7 @@ public interface TodoMapper {
     List<Todo> allTodo();
     @Select("SELECT * FROM todo WHERE date_time=#{dateTime}")
     List<Todo> todoList(String dateTime);
-    @Insert("INSERT INTO todolist (date_time, title, content, created_at) values (#{dateTime}, #{title}, #{content}, NOW())")
+    @Insert("INSERT INTO todo (date_time, title, content, created_at) values (#{dateTime}, #{title}, #{content}, NOW())")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void addTodo(Todo todo);
     @Update("UPDATE todo SET date_time=#{dateTime}, title=#{title}, content=#{content} WHERE id=#{id}")
--- src/main/java/com/work/todolist/todolist/service/TodoService.java
+++ src/main/java/com/work/todolist/todolist/service/TodoService.java
@@ -1,7 +1,33 @@
 package com.work.todolist.todolist.service;
 
+import com.work.todolist.todolist.domain.Todo;
+import com.work.todolist.todolist.mapper.TodoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TodoService {
+    @Autowired
+    TodoMapper todoMapper;
+    public List<Todo> allTodo() {
+        return todoMapper.allTodo();
+    }
+
+    public List<Todo> getTodo(String todo) {
+        return todoMapper.todoList(todo);
+    }
+
+    public void addTodo(Todo todo) {
+        todoMapper.addTodo(todo);
+    }
+
+    public void updateTodo(Todo todo) {
+        todoMapper.updateTodo(todo);
+    }
+
+    public void deleteTodo(Long id) {
+        todoMapper.deleteTodo(id);
+    }
 }
